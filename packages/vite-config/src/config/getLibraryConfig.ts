259f897d98fb78bc024438f260353888a7a850f1
kkirss <kristofer.kirss@hotmail.com>
2023-09-24 06:46:57 +0300

    feat: Use tsc for type checking
    For some reason, vite-plugin-dts was not reporting type errors.
    Thus, we need to use tsc for it.
    
    feat: Export types from src directly.
    
    This allows building without generating types first, improving performance.

--- packages/vite-config/src/config/getLibraryConfig.ts
+++ packages/vite-config/src/config/getLibraryConfig.ts
@@ -1,13 +1,8 @@
 import { type UserConfig } from 'vite'
 import { externalizeDeps } from 'vite-plugin-externalize-deps'
-import dts from 'vite-plugin-dts'
 
 export const getLibraryConfig = (): UserConfig => ({
   plugins: [
-    dts({
-      entryRoot: 'src',
-      insertTypesEntry: true
-    }),
     externalizeDeps({
       deps: false
     })
