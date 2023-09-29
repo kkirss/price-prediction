2df1f4afb77e21f70e99fa6e44be3426a6c5a01d
kkirss <kristofer.kirss@hotmail.com>
2023-09-29 15:34:01 +0300

    Updates

---
+++ assets/@price-prediction/coincap-api-2fb500a7.js
@@ -0,0 +1,1 @@
+import{c as t}from"../openapi-fetch-6bcd5f47.js";const c="/v2/assets/{id}",a="https://api.coincap.io",e=(s={})=>t({baseUrl:a,...s});export{e as A,c as s};
--- assets/Home-2f22f08d.js
+++
@@ -1,1 +0,0 @@
-import{t}from"./@solidjs/router-a46c8cdf.js";import"./solid-js-1e500652.js";const o=t("<section><h1>Hello World!"),m=()=>o();export{m as default};
