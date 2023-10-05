28cd10b9aac98b8a333b729799c082573a8b4b91
kkirss <kristofer.kirss@hotmail.com>
2023-10-05 23:52:20 +0300

    feat: Export API schema objects as named exports

---
+++ packages/api-client/src/openAPI/models.ts
@@ -0,0 +1,8 @@
+import { type components } from './schema'
+
+export type ValidationErrorItem = components['schemas']['ValidationErrorItem']
+// export type Error = components['schemas']['Error'];
+export type SignupRequest = components['schemas']['SignupRequest']
+export type LoginRequest = components['schemas']['LoginRequest']
+export type User = components['schemas']['User']
+export type Session = components['schemas']['Session']
