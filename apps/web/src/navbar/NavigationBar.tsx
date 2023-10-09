a6c07d640c47642eb37cd150a21118d83736a2a3
kkirss <kristofer.kirss@hotmail.com>
2023-10-09 03:25:24 +0300

    feat: Clicking on navbar header navigates back to home page

--- apps/web/src/navbar/NavigationBar.tsx
+++ apps/web/src/navbar/NavigationBar.tsx
@@ -1,20 +1,38 @@
-import { Component } from 'solid-js'
-import { AppBar, Toolbar, Typography } from '@suid/material'
+import { Component, Show } from 'solid-js'
+import { AppBar, Box, Link, Toolbar, Typography } from '@suid/material'
+import { A, useLocation } from '@solidjs/router'
 
 import { ToggleDarkMode } from '@price-prediction/ui-theme'
 
 import AuthInfo from '~/auth/AuthInfo'
 
-const NavigationBar: Component = () => (
+const NavigationBarHeader: Component = () => {
+  const location = useLocation()
+  const header = 'Price Prediction'
+  return (
+    <Show
+      when={location.pathname !== '/'}
+      fallback={
+        <Typography variant='h6'>
+          {header}
+        </Typography>
+      }
+    >
+      <Link href='/' component={A} variant='h6'>
+        {header}
+      </Link>
+    </Show>
+  )
+}
+
+const NavigationBar: Component = () =>
   <AppBar position='static'>
     <Toolbar>
-      <Typography variant='h6' sx={{ flexGrow: 1 }}>
-        Price Prediction
-      </Typography>
+      <NavigationBarHeader />
+      <Box sx={{ flexGrow: 1 }} />
       <AuthInfo />
       <ToggleDarkMode sx={{ ml: 1 }} />
     </Toolbar>
   </AppBar>
-)
 
 export default NavigationBar
