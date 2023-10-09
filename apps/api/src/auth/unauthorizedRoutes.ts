593c644f96aae6dbd6eb7fbdd76c782d974bffb0
kkirss <kristofer.kirss@hotmail.com>
2023-10-09 07:07:35 +0300

    feat: Handle registering unauthorized routes with path params

--- apps/api/src/auth/unauthorizedRoutes.ts
+++ apps/api/src/auth/unauthorizedRoutes.ts
@@ -1,8 +1,26 @@
 const unauthorizedRoutes = new Set<string>([])
+const unauthorizedRouteRegexps = new Set<RegExp>([])
+
+// TODO: Using regex to match path params is not ideal. We should use a library.
+const pathParamRegex = /\/(:\w+)/g
 
 export const registerUnauthorizedRoute = (path: string): void => {
+  if (pathParamRegex.test(path)) {
+    const pathRegexp = new RegExp(path.replace(pathParamRegex, '/\\w+'))
+    unauthorizedRouteRegexps.add(pathRegexp)
+  }
   unauthorizedRoutes.add(path)
 }
 
-export const isUnauthorizedRoute = (path: string): boolean =>
-  unauthorizedRoutes.has(path)
+export const isUnauthorizedRoute = (path: string): boolean => {
+  const contains = unauthorizedRoutes.has(path)
+  if (contains) {
+    return true
+  }
+  for (const pathRegexp of unauthorizedRouteRegexps) {
+    if (pathRegexp.test(path)) {
+      return true
+    }
+  }
+  return false
+}
